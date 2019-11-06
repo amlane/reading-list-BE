24938ce068b35840f81d8e0859320f68a5d0aabc
amlane <twelvexstring@gmail.com>
2019-11-05 23:20:33 -0600

    installed cors

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema.js");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+// allow cross origin requests
+app.use(cors());
+
 // connect to Atlas MongoDB
-mongoose.connect("mongodb+srv://admin:Cr41.80ab@gql-reading-list-zxyb8.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://admin:Cr41.80ab@gql-reading-list-zxyb8.mongodb.net/test?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true })
 mongoose.connection.once("open", () => {
     console.log("connected to database")
 });
